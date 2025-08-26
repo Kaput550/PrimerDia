092fd66f69c2aa58694f8af31d14d31db80b2fd2
Kaput550 <jisaacgm7@gmail.com>
2025-08-26 13:33:19 -0600

    Inicio

--- app/src/main/java/com/example/android_primerdia/MainActivity.kt
+++ app/src/main/java/com/example/android_primerdia/MainActivity.kt
@@ -14,6 +14,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.android_primerdia.ui.theme.AndroidPrimerDiaTheme
 
+//JONATHAN ISAAC GARCIA MIJARES
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
